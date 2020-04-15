95e472062c017935301991fb910ab07e42b035c0
xiaoliang1111@qq.com <xiaoliang1111@outlook.com>
2020-04-15 17:10:10 +0800

    first

---
+++ jump7.py
@@ -0,0 +1,10 @@
+for i in range(100):
+	if int(i/7)==i/7:
+		continue
+	elif (i-int(i/10)*10)==7:
+		continue
+	elif int(i/10)==7:
+		continue
+	else:
+		 print(i)
+                                                      
